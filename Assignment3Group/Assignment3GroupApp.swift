5115b813e8c7200d0074591873041453c2543683
Kundan Rayamajhi <kundon1121@gmail.com>
2024-05-03 10:24:46 +1000

    Initial Commit

---
+++ Assignment3Group/Assignment3GroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3GroupApp.swift
+//  Assignment3Group
+//
+//  Created by kundan rayamajhi on 3/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3GroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment3Group/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment3Group
+//
+//  Created by kundan rayamajhi on 3/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
